1a0ef6901cdb56e92d4b7569cf10e2aeee401437
Chiang Fong Lee <myself@cflee.net>
2015-02-09 23:05:11 +0800

    Convert lab3 code to LF line endings
    Previously was in CRLF, inherited from the original provided source files.

--- lab3/labq3_utility.rb
+++ lab3/labq3_utility.rb
@@ -1,7 +1,7 @@
 # labq3_utility.rb
 # Do not submit this file
 
-# Do not modify this file as well. 
+# Do not modify this file as well.
 # Ensure that this file is in the same folder as labq3_main.rb
 
 # takes in an array of strings and returns the same array with all the strings converted to integers
@@ -26,33 +26,33 @@ end
 
 # takes in 2 arguments:
 #   - selected is an array of 5 integers (user IDs)
-#   - followers is a 2D array of followers 
+#   - followers is a 2D array of followers
 # returns an array of unique followers for the 5 selected users in sorted order
 def get_unique_followers (selected, followers)
   f0 = followers[selected[0].to_i]
   if (f0 == nil)
     f0 = []
-  end 
+  end
   f1 = followers[selected[1].to_i]
   if (f1 == nil)
     f1 = []
-  end 
+  end
   f2 = followers[selected[2].to_i]
   if (f2 == nil)
     f2 = []
-  end 
+  end
   f3 = followers[selected[3].to_i]
   if (f3 == nil)
     f3 = []
-  end 
+  end
   f4 = followers[selected[4].to_i]
   if (f4 == nil)
     f4 = []
-  end 
-  
+  end
+
   f = (f0 + f1 + f2 + f3 + f4).uniq
   # remove original users (in selected)
-  f.delete(selected[0].to_i) 
+  f.delete(selected[0].to_i)
   f.delete(selected[1].to_i)
   f.delete(selected[2].to_i)
   f.delete(selected[3].to_i)
